a981e1a61c120061e74787ee5856fc17bb2e3fe9
dzhan008 <dzhan008@ucr.edu>
2017-12-20 21:09:58 -0800

    Started Stats; Organized Folders
    -Started a base stats class for entities
    -Move resources away from scripts and added a prefab/models folder

---
+++ Assets/Scripts/Stats.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Stats class defining the basic properties of an entity.
+/// </summary>
+public class Stats : MonoBehaviour {
+
+    //Health and Ultimate Bar
+    public int Health
+    {
+        get
+        {
+            return _Health;
+        }
+        set
+        {
+            if(value < 0)
+            {
+                _Health = 0;
+            }
+            else
+            {
+                _Health = value;
+            }
+        }
+    }
+    private int _Health;
+
+    public int ultimateMeter
+    {
+        get
+        {
+            return _ultimateMeter;
+        }
+        set
+        {
+            if(value < 0)
+            {
+                _ultimateMeter = 0;
+            }
+            else
+            {
+                _ultimateMeter = value;
+            }
+        }
+    }
+    private int _ultimateMeter;
+
+
+    //General Stats
+    private int Strength;
+    private int Agility;
+    private int Intelligence;
+
+    //Damage
+    private int minDamage;
+    private int maxDamage;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public int CalculateDamage()
+    {
+        return Random.Range(minDamage, maxDamage + 1);
+    }
+}
