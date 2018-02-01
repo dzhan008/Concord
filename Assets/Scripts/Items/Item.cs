2d6a3bb28ef706400e63067f4a8723ceb0e9533b
Emmilio <esego001@ucr.edu>
2018-01-31 21:29:16 -0800

    Added weapon stuff and prefabs

---
+++ Assets/Scripts/Items/Item.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// The list of playable roles such as warrior, mage, etc.
+public enum Role
+{
+    warrior = 0,
+    In_Game,
+    Cutscene
+}
+
+public class Item : MonoBehaviour {
+
+}
---
+++ Assets/Scripts/Items/Weapon.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : Item {
+
+    // Weilder must be one of these roles to weild this weapon
+    [SerializeField]
+    private Role RoleRestriction;
+
+    // Weilder must be at least this player level to weild this weapon
+    [SerializeField]
+    private int minPlayerLevel;
+
+    // A reference to the animation that should be played when this weapon is held and REGULAR attack is pressed
+    [SerializeField]
+    private Animation normalAttackAnimation;
+
+    // A reference to the animation that should be played when this weapon is held and SUPER attack is pressed
+    [SerializeField]
+    private Animation superAttackAnimation;
+
+    public bool CanWeild(Player weilder) {
+        if (weilder.currentRole == RoleRestriction 
+            && weilder.PlayerLevel >= minPlayerLevel) {
+            return true;
+        }
+        return false;
+    }
+	
+}
