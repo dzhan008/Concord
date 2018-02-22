ad9ac519228c4612e8d815bc25d46a61c1d4ed4f
nadnbuds <lalam003@ucr.edu>
2018-02-22 01:55:06 -0800

    Moved files and set up testing scene

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -12,14 +12,30 @@ public enum GameState
 
 public class GameManager : MonoBehaviour {
 
+    Trigger.OnTrigger triggerHit;
+
     [SerializeField]
     private Player testPlayer;
+
+    [SerializeField]
+    private Transform startingPos;
+
+    [SerializeField]
+    private Trigger endingPos;
 
     GameState state;
 
     private void Awake()
     {
+        triggerHit = endLevel;
+        endingPos.Initialize(triggerHit);
+        testPlayer.transform.position = startingPos.position;
         //To-do Change this to instantiate all players on start so IDs can correctly be set
         Blackboard.setPlayerRef(testPlayer, 0);
+    }
+
+    private void endLevel()
+    {
+        Time.timeScale = 0;
     }
 }
